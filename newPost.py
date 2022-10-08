29ea93cc6a1917cd4c868562b38c33a3f60cd512
龙诺明 <differlong@gmail.com>
2022-10-08 18:58:32 +0800

    中国是部金融史读后感

--- newPost.py
+++ newPost.py
@@ -3,7 +3,10 @@ import time
 
 name = input("输入文件名: ")
 name = name.replace(" ", "")
-order = "hugo new post/" + name + ".md"
+if ".md" not in name:
+    name = name + + ".md"
+
+order = "hugo new post/" + name
 print(order)
 os.system(order)
 print("success!!!")
